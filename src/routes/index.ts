939a627a2b40ca58b781950b3964931b2764eb3b
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2024-03-08 14:19:12 -0300

    feat: protect with token JWT user and products routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import userRoutes from "./user.routes";
 import productRoutes from "./product.routes";
+import authRoutes from "./auth.routes";
 
 const routes = (app: any) => {
   userRoutes(app);
   productRoutes(app);
+  authRoutes(app);
 };
 
 export default routes;
